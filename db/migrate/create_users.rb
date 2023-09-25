73b720f6b6507978904d4aae3d2b7da011d4c757
Ben Purinton <ben.purinton@gmail.com>
2023-09-25 13:45:24 -0800

    remove username from users table

--- db/migrate/create_users.rb
+++ db/migrate/create_users.rb
@@ -2,7 +2,6 @@ require "sinatra/activerecord"
 require "./config/environment"
 
 ActiveRecord::Migration.create_table :users do |t|
-  t.string :username
   t.string :email
   t.string :password_digest 
   t.timestamps
